cf4415f8d856c880cf14994d6930fe1b8558241d
rafaelfgx <rafaelfgx@gmail.com>
2025-04-24 09:04:57 -0300

    .

---
+++ Blockchain/Program.cs
@@ -0,0 +1,41 @@
+namespace Blockchain;
+
+public class Program
+{
+    public static void Main()
+    {
+        var miner = Guid.NewGuid();
+
+        var address1 = Guid.NewGuid();
+
+        var address2 = Guid.NewGuid();
+
+        var address3 = Guid.NewGuid();
+
+        var blockchain = new Blockchain();
+
+        blockchain.Add(new(address1, address2, 10));
+
+        blockchain.Mine(miner);
+
+        blockchain.Add(new(address1, address3, 20));
+
+        blockchain.Add(new(address2, address3, 30));
+
+        blockchain.Mine(miner);
+
+        Console.WriteLine(blockchain + Environment.NewLine);
+
+        #region Tampering Attempt
+
+        Console.WriteLine("[TAMPERING ATTEMPT]" + Environment.NewLine);
+
+        blockchain.Blocks[1] = new(blockchain.Blocks[0], [new(address1, miner, 40)]);
+
+        Console.WriteLine(blockchain + Environment.NewLine);
+
+        #endregion Tampering Attempt
+
+        Console.ReadKey();
+    }
+}
---
+++ Solidity/Pokemon/contracts/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+string constant BASE_URI = "https://ipfs.io/ipfs/";
+
+uint8 constant EXPERIENCE_INCREMENT = 50;
+
+uint8 constant EXPERIENCE_TO_NEXT_LEVEL = 100;
